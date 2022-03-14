e1a7365251b16601ab9b1d15f7ad4da40546484d
ziwei <626398871@qq.com>
2022-03-14 19:09:46 +0800

    mysql.go finish

--- dao/mysql/mysql.go
+++ dao/mysql/mysql.go
@@ -1,1 +1,39 @@
 package mysql
+
+import (
+	"fmt"
+	"ziweiMemo/settings"
+
+	"go.uber.org/zap"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+var db *sqlx.DB
+
+func Init(cfg *settings.MySQLConfig) (err error) {
+	// root:123456@tcp(127.0.0.1:3306)/ziweiMemo?charset=utf8mb4&parseTime=True
+	dsn := fmt.Sprintf("%s:%s@tcp(%s%s)/%s?charset=utf8mb4&parseTime=True",
+		cfg.User,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+		cfg.DbName,
+	)
+
+	// 连接mysql
+	db, err = sqlx.Connect("mysql", dsn)
+	if err != nil {
+		zap.L().Error("[package: mysql] [func: Init] [sqlx.Connect(\"mysql\", dsn)] failed, err: %v\n", zap.Error(err))
+		return
+	}
+
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
+	return
+}
+
+func Close() {
+	_ = db.Close()
+}
--- go.mod
+++ go.mod
@@ -6,8 +6,10 @@ require (
 	github.com/fsnotify/fsnotify v1.5.1
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-playground/validator/v10 v10.10.1 // indirect
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jmoiron/sqlx v1.3.4
 	github.com/magiconair/properties v1.8.6 // indirect
-	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/viper v1.10.1
 	github.com/ugorji/go v1.2.7 // indirect
@@ -17,4 +19,5 @@ require (
 	golang.org/x/crypto v0.0.0-20220313003712-b769efc7c000 // indirect
 	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
