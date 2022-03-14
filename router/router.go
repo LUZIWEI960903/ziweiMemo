8a6ce8df0c204e93dab07994f6478268d265c91b
ziwei <626398871@qq.com>
2022-03-14 21:14:35 +0800

    router.go finish

--- router/router.go
+++ router/router.go
@@ -1,1 +1,18 @@
 package router
+
+import (
+	"net/http"
+	"ziweiMemo/logger"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetUp(cfgMode string) *gin.Engine {
+	r := gin.New()
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, cfgMode)
+	})
+	return r
+}
