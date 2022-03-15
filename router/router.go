c78794d24cdf39b03140909bf32af75f8f5eff09
ziwei <626398871@qq.com>
2022-03-15 15:27:23 +0800

    fix: controllers/user.go

--- router/router.go
+++ router/router.go
@@ -1,18 +1,25 @@
 package router
 
 import (
-	"net/http"
+	"ziweiMemo/controllers"
 	"ziweiMemo/logger"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetUp(cfgMode string) *gin.Engine {
+	if cfgMode == gin.ReleaseMode {
+		gin.SetMode(gin.ReleaseMode) // 设置发布模式
+	}
+
 	r := gin.New()
 	r.Use(logger.GinLogger(), logger.GinRecovery(true))
 
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, cfgMode)
-	})
+	// v1 版本
+	v1 := r.Group("/api/v1")
+
+	// 注册功能
+	v1.POST("/register", controllers.UserRegisterHandler)
+
 	return r
 }
