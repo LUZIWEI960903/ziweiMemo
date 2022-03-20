5f58a58d4e8b07cdeeb25e4520b77e3b4be6ced7
ziwei <626398871@qq.com>
2022-03-20 16:15:32 +0800

    feat: TestCreateTask testing successgit status

---
+++ dao/mysql/task_test.go
@@ -0,0 +1,25 @@
+package mysql
+
+import (
+	"testing"
+	"time"
+	"ziweiMemo/models"
+)
+
+func TestCreateTask(t *testing.T) {
+	task := &models.Task{
+		TaskID:     123,
+		UserId:     123,
+		Status:     0,
+		Title:      "just a test",
+		Content:    "test test test~~",
+		StartTime:  time.Time{},
+		EndTime:    time.Time{},
+		CreateTime: time.Time{},
+		UpdateTime: time.Time{},
+	}
+	if err := CreateTask(task); err != nil {
+		t.Fatal("failed~~")
+	}
+	t.Log("testing success!!")
+}
